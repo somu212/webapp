bc84293ac9cf3f4927cdc807f0de842e45360070
SOUMYADIP DAS <dassoumyadip890@gmail.com>
2026-02-12 21:19:26 +0530

    forth commit

--- obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62527254bbcd3bac352e9a22486347135aa9c816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0feadf8f7dfd16c957c3e8f4eacfccd3cbce4b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
